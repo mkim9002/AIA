401ed59d0e3339bfbef943b1237bf17dfa9c5b06
mkim9002 <126454272+mkim9002@users.noreply.github.com>
2023-03-23 20:38:26 +0900

    123

--- keras/keras43_split2_LSTM.py
+++ keras/keras43_split2_LSTM.py
@@ -1,72 +1,62 @@
 import numpy as np
-import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, SimpleRNN, LSTM, GRU
-from tensorflow.python.keras.callbacks import EarlyStopping
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, LSTM
+from tensorflow.keras. callbacks import EarlyStopping
 
-
+#1. data
 dataset = np.array(range(1, 101))
 timesteps = 5
-x_predict = np.array(range(96, 106)) #100-106 예상값
-# 96,97,98,99
-# 97,98,99,100
-# 98,99,100,101
-# ...
-# 102,103,104,105
+x_predict = np.array(range(96, 106))
+
+#100-106 예상값 ,시계열 데이터
 
 def split_x(dataset, timesteps):
-    aaa = []
-    for i in range(len(dataset) - timesteps +1):
-        subset = dataset[i : (i+timesteps)]
-        aaa.append(subset)
-    return np.array(aaa)
+    gen=(dataset[i : (i + timesteps)] for i in range(len(dataset)- timesteps +1))
+    return np.array(list(gen))
+
+bbb = split_x(dataset, timesteps)
+print(bbb)
+print(bbb.shape)
+
+x = bbb[:, :-1]
+y = bbb[:, 4:5]
+
+print(x.shape)
+print(y.shape)
 
 a = split_x(x_predict, timesteps)
 print(a)
 
-x = a[: , :4]
-print(x)
-print(x.shape) #(6, 4)
-y = a[:,-1]
-print(y)
+a1 = a[:, :4]
+print(a1)
 
-print(x.shape) #(6, 4)
-print(y.shape)
+x = x.reshape(96, 4, 1)
 
-bbb = split_x(dataset, timesteps)
-print(bbb)
-print(bbb.shape) #(96, 5)
-
-a = bbb[:, :4]
-b = bbb[:,-1]
-
-print(a.shape) #(96, 4)
-print(y.shape) #(96,)
-
-a = a.reshape(96,4,1)
-x = x.reshape(6,4,1)
-
+a1 = a1.reshape(6, 4 , 1)
 
 #2. model
 model = Sequential()
-model.add(LSTM(10, input_shape=(4,1),return_sequences=True))
-model.add(LSTM(11))
-#model.add(GRU(12))
+model.add(LSTM(64, input_shape=(4,1), activation='linear', recurrent_activation='tanh'))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(10))
 model.add(Dense(10))
 model.add(Dense(10))
 model.add(Dense(1))
 
-model.summary()
-
-
-
-#3. compile, loss,model.fit, epochs
+#3.compile.practice
 model.compile(loss='mse', optimizer='adam')
 es = EarlyStopping(monitor='loss', mode='min', patience=100, restore_best_weights=True)
-model.fit(a,b, epochs=100, callbacks=[es])
+model.fit(x,y, epochs=100, callbacks=[es])
 
-#4. loss,predict
+#4. evaluate, predict
+loss = model.evaluate(x,y)
+a1 = a1.reshape(6, 4 ,1)
 
-result = model.predict(x)
+result = model.predict(a1)
+print('loss :', loss)
+print('result :', result)
 
-print('result : ', result)
+    
+    
+    
