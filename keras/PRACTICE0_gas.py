42bd36a5a5c7563aaa8a042b78582f7bcc7b80de
mkim9002 <126454272+mkim9002@users.noreply.github.com>
2023-04-13 17:01:42 +0900

    Update PRACTICE0_gas.py

--- keras/PRACTICE0_gas.py
+++ keras/PRACTICE0_gas.py
@@ -1,78 +1,73 @@
-import numpy as np
 import pandas as pd
-from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Dense, LSTM, GRU, Conv1D, Flatten, MaxPooling2D, Input, Concatenate,MaxPooling1D
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import r2_score, mean_squared_error
-import matplotlib.pyplot as plt
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.metrics import f1_score, make_scorer, accuracy_score
 
-# 1. data
-# 1.1 path, path_save, read_csv
-path = './_data/개인프로젝트/'
-path_save = './_save/개인프로젝트/'
 
-datasets_검색수 = pd.read_csv(path + '한국관광공사_검색수.csv', index_col=0, encoding='cp949')
-datasets_방문자수 = pd.read_csv(path + '한국관광공사_방문자수.csv', index_col=0, encoding='cp949')
+# 훈련 데이터 및 테스트 데이터 로드
+path = 'd:/study_data/_data/gas/'
+save_path = 'd:/study_data/_save/gas/'
+train_data = pd.read_csv(path+'train_data.csv')
+test_data = pd.read_csv(path+'test_data.csv')
+submission = pd.read_csv(path+'answer_sample.csv')
 
-# 2. 전처리
-# 2.1. 두 데이터프레임을 시도명을 기준으로 병합
-df = pd.merge(datasets_검색수, datasets_방문자수, on='시도명')
 
-# 2.2. 컬럼명 변경 (년도는 뒤에 두 자리만 사용)
-df.columns = ['시도명', 'search_19', 'search_20', 'search_21', 'search_22', 'visit_18', 'visit_19', 'visit_20', 'visit_21', 'visit_22']
+# Combine train and test data
+data = pd.concat([train_data, test_data], axis=0)
 
-# 2.3. 시도명을 인덱스로 변경
-df = df.set_index('시도명')
+# Preprocess data
+def type_to_HP(type):
+    HP=[30,20,10,50,30,30,30,30]
+    gen=(HP[i] for i in type)
+    return list(gen)
+train_data['type'] = type_to_HP(train_data['type'])
+test_data['type'] = type_to_HP(test_data['type'])
 
-# 3. 모델 구성
-# 모델 구성을 위해 필요한 라이브러리 import
-from tensorflow.keras.layers import Input, Dense, Concatenate
 
-# 모델 구성
-input_ = Input(shape=(10,))
-x1 = Dense(64, activation='relu')(input_)
-x1 = Dense(32, activation='relu')(x1)
-x1 = Dense(16, activation='relu')(x1)
+# Select subset of features for Autoencoder model
+features = ['air_inflow', 'air_end_temp', 'motor_current', 'motor_rpm', 'motor_temp', 'motor_vibe']
 
-x2 = Conv1D(32, 2, activation='relu', padding='same')(input_)
-x2 = MaxPooling1D(pool_size=2)(x2)
-x2 = Conv1D(64, 2, activation='relu', padding='same')(x2)
-x2 = MaxPooling1D(pool_size=2)(x2)
-x2 = Flatten()(x2)
+# Split data into train and validation sets
+x_train, x_val = train_test_split(data[features], train_size=0.9, random_state=555)
 
-concat = Concatenate()([x1, x2])
-output = Dense(1)(concat)
-model = Model(inputs=input_, outputs=output)
+# Normalize data
+scaler = MaxAbsScaler()
+x_train = scaler.fit_transform(x_train)
+x_val = scaler.transform(x_val)
 
-# 4. 컴파일, 훈련
-# 컴파일
-model.compile(loss='mse', optimizer='adam')
+# Define Autoencoder model
+input_layer = Input(shape=(len(features),))
+encoder = Dense(4, activation='relu')(input_layer)
+decoder = Dense(len(features), activation='sigmoid')(encoder)
+autoencoder = Model(inputs=input_layer, outputs=decoder)
 
-# 훈련 데이터 준비
-X = df.values[:, :10] # 검색수와 방문자수
-y = df.values[:, -1] # 2022 방문자수
+# Compile Autoencoder model
+autoencoder.compile(optimizer='adam', loss='mean_squared_error')
 
-# 훈련
-history = model.fit(X, y, epochs=200, verbose=0)
+# Train Autoencoder model
+es = EarlyStopping(monitor='val_loss', mode='auto', verbose=1, patience=60)
+autoencoder.fit(x_train, x_train, epochs=3000, batch_size=20, validation_data=(x_val, x_val), callbacks=[es])
 
-#예측값 구하기
-y_pred = model.predict(X)
+# Predict anomalies in test data
+test_data = scaler.transform(test_data[features])
+predictions = autoencoder.predict(test_data)
+mse = ((test_data - predictions) ** 2).mean(axis=1)
 
-#도표 만들기
-import matplotlib.pyplot as plt
-import numpy as np
-plt.rcParams['font.family']='Malgun Gothic'
-plt.figure(figsize=(15,5))
-plt.plot(df.index, y, 'o-', label='실제값')
-plt.plot(df.index, y_pred, 'o-', label='예측값')
-plt.legend()
-plt.title('2023년 방문자수 예측')
-plt.xlabel('시도명')
-plt.ylabel('방문자수')
-plt.xticks(rotation=45)
-plt.show()
+# Use LOF to detect anomalies
+clf = LocalOutlierFactor(n_neighbors=20, contamination=0.1)
+binary_predictions = [1 if x == -1 else 0 for x in clf.fit_predict(test_data)]
 
-#각 시도별 2023년 방문자수 예측
-print(model.predict(X)[-1]) # 제주도의 2023년 방문자수 예측 값 출력
+# Combine predictions from Autoencoder and LOF
+final_predictions = [1 if x+y >= 1 else 0 for x, y in zip(binary_predictions, mse > (mse.mean() + mse.std() * 2))]
+
+submission['label'] = pd.DataFrame({'Prediction': final_predictions})
+
+# Save submission file
+import datetime 
+date = datetime.datetime.now()  
+date = date.strftime("%m%d_%H%M")
+submission.to_csv(save_path + date + 'submit.csv', index=False)
