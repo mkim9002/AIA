b23883f6bfa29295960fc96d91bccbe1ca755e08
mkim9002 <mkim9002@gmail.com>
2023-04-21 14:46:45 +0900

    234
    rteet

--- keras/PRACTICE0_gas.py
+++ keras/PRACTICE0_gas.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import datetime
-from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.pipeline import make_pipeline
 
 # 훈련 데이터 및 테스트 데이터 로드
 path = 'd:/study_data/_data/gas/'
@@ -17,47 +18,53 @@ def type_to_HP(type):
     gen=(HP[i] for i in type)
     return list(gen)
 
-train_data['type']=type_to_HP(train_data['type'])
-test_data['type']=type_to_HP(test_data['type'])
+train_data['type'] = type_to_HP(train_data['type'])
+test_data['type'] = type_to_HP(test_data['type'])
 
 # Select subset of features for LOF model
 features = ['air_inflow', 'air_end_temp', 'out_pressure', 'motor_current', 'motor_rpm', 'motor_temp', 'motor_vibe']
-
-from sklearn.ensemble import IsolationForest
-from sklearn.model_selection import RandomizedSearchCV
 
 # Prepare train and test data
 X = train_data[features]
 
 # Split data into train and validation sets
-X_train, X_val = train_test_split(X, train_size=0.9999999999, random_state=1)
+X_train, X_val = train_test_split(X, train_size=0.9999993459999, random_state=174)
 
 # Normalize data
 scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 X_val = scaler.transform(X_val)
 
-# Tune Isolation Forest model
-if_model = IsolationForest(random_state=1)
-param_distributions = {
-    'n_estimators': [50, 100, 200],
-    'max_samples': [0.1, 0.5, 1.0],
-    'contamination': ['auto', 0.01, 0.05, 0.1]
-}
-if_search = RandomizedSearchCV(if_model, param_distributions, n_iter=10, random_state=1)
-if_search.fit(X_train)
+# Apply Local Outlier Factor
+lof = LocalOutlierFactor(n_neighbors=1, contamination=0.1)
+y_pred_train = lof.fit_predict(X_train)
 
-# Print best hyperparameters
-print(if_search.best_params_)
+# Tuning: Adjust the n_neighbors and contamination parameters
+lof_tuned = LocalOutlierFactor(n_neighbors=35, contamination=0.047)
+y_pred_train_tuned = lof_tuned.fit_predict(X_train)
 
-# Predict anomalies in test data using tuned Isolation Forest
-test_data_if = scaler.transform(test_data[features])
-y_pred_test_if = if_search.predict(test_data_if)
-if_predictions = [1 if x == -1 else 0 for x in y_pred_test_if]
+# Predict anomalies in test data using tuned LOF
+lof_pipeline = make_pipeline(MinMaxScaler(), lof_tuned)
+y_pred_test_lof = lof_pipeline.fit_predict(test_data[features])
+lof_predictions = [1 if x == -1 else 0 for x in y_pred_test_lof]
 
-submission['label'] = pd.DataFrame({'Prediction': if_predictions})
+submission['label'] = pd.DataFrame({'Prediction': lof_predictions})
 print(submission.value_counts())
 
-# Save submission file
-date = datetime.datetime.now().strftime("%m%d_%H%M")
+#time
+date = datetime.datetime.now()
+date = date.strftime("%m%d_%H%M")
 submission.to_csv(save_path + date + 'submission.csv', index=False)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+print(test_data.corr())
+plt.figure(figsize=(10, 8))
+sns.set(font_scale=1.2)
+sns.heatmap(train_data.corr(),square=True, annot=True, cbar=True)
+plt.show()
+
+import calendar
+print(calendar.calendar(2023))
+calendar.prmonth(2023, 4)
