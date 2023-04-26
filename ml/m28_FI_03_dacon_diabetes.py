17e5573b23e1439b64ba320cf9b95bb016551c75
mkim9002 <mkim9002@gmail.com>
2023-04-26 11:14:30 +0900

    2525
    2tw

---
+++ ml/m28_FI_03_dacon_diabetes.py
@@ -0,0 +1,86 @@
+#실습
+#피쳐 임포턴스 가 전체 중요도에서  하위 20-25% 컬럼제거
+#재구성 후
+#모델을 돌려서 결과 도출
+#기존 모델들과 성능비교
+
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+
+#1. 데이터 
+path = './_data/dacon_diabetes/'
+path_save = './_save/dacon_diabetes/'
+
+train_csv= pd.read_csv(path+'train.csv', index_col=0)
+test_csv= pd.read_csv(path+'test.csv', index_col=0)
+x = train_csv.drop(['Outcome'], axis=1)
+y = train_csv['Outcome']
+
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, shuffle=True, random_state=42, test_size=0.2, stratify=y
+)
+
+#2. 모델 구성
+models = [DecisionTreeClassifier(), RandomForestClassifier(), GradientBoostingClassifier(), XGBClassifier()]
+
+#3. 컴파일, 훈련 및 평가
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 10))
+ax = axes.ravel()
+
+for i, model in enumerate(models):
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    acc = accuracy_score(y_test, y_pred)
+    print(f"{model.__class__.__name__} accuracy_score: {acc:.4f}")
+    print(f"{model.__class__.__name__} feature importances:", model.feature_importances_)
+
+    # 그림그리기
+    n_features = datasets.data.shape[1]
+    ax[i].barh(np.arange(n_features), model.feature_importances_, align='center')
+    ax[i].set_yticks(np.arange(n_features))
+    ax[i].set_yticklabels(datasets.feature_names)
+    ax[i].set_xlabel('Feature Importances')
+    ax[i].set_ylabel('Features')
+    ax[i].set_ylim(-1, n_features)
+    ax[i].set_title(model.__class__.__name__)
+
+plt.show()
+
+#4. 모델 재학습 및 평가
+x_train_imp = x_train[:,model.feature_importances_ > np.percentile(model.feature_importances_, 25)] 
+x_test_imp = x_test[:,model.feature_importances_ > np.percentile(model.feature_importances_, 25)] 
+
+for i, model in enumerate(models):
+    model.fit(x_train_imp, y_train)
+    y_pred = model.predict(x_test_imp)
+    acc = accuracy_score(y_test, y_pred)
+    print(f"{model.__class__.__name__} accuracy_score: {acc:.4f}")
+
+
+
+#결과비교
+# 예)
+#1. DecesionTree
+#기존 acc : 0.9298
+# 컬럼삭제후 acc :0.9298
+
+#2. RandomForest
+#기존 acc : 0.9561
+# 컬럼삭제후 acc :0.9561
+
+#3. GradientDecentBoosting
+#기존 acc : 0.9825
+# 컬럼삭제후 acc :0.9737
+
+#4. XGBoost
+#기존 acc : 0.9737
+# 컬럼삭제후 acc :0.9737
+
---
+++ ml/m39_outlier2_반장.py
@@ -0,0 +1,24 @@
+import numpy as np
+# aaa = np.array([-10,2,3,4,5,6,7,8,9,10,11,12,50])
+aaa = np.array([2,3,4,5,6,7,8,-10,9,10,11,12,50])
+
+
+def outliers(data_out):
+    quartile_1, q2, quartile_3 = np.percentile(data_out,
+                                              [25, 50, 75])
+    print("1사분위 :", quartile_1)
+    print("q2 :", q2)
+    print("3사분위 :", quartile_3)
+    iqr = quartile_3 - quartile_1
+    print("iqr : ",iqr)
+    lower_bound = quartile_1 - (iqr * 1.5) # -5
+    upper_bound = quartile_3 + (iqr * 1.5) # 19
+    return np.where((data_out>upper_bound) |
+                    (data_out<lower_bound)) # 19보다 크거나, -5 보다 작은놈
+    
+outliers_loc = outliers(aaa)
+print("이상한 숫자 위치 :", outliers_loc)
+
+import matplotlib.pyplot as plt
+plt.boxplot(aaa)
+plt.show()
