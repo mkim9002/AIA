5a62399a14a0102ba3f05a3a3b5e1ffe89ef1884
mkim9002 <mkim9002@gmail.com>
2023-04-26 12:38:25 +0900

    3445
    3636

--- ml/m39_outlier3.py
+++ ml/m39_outlier3.py
@@ -14,15 +14,26 @@ import numpy as np
 import pandas as pd
 
 def outliers(data_out):
-    quartile_1, q2, quartile_3 = np.percentile(data_out, [25, 50, 75])
-    iqr = quartile_3 - quartile_1
-    lower_bound = quartile_1 - (iqr * 1.5) # -5
-    upper_bound = quartile_3 + (iqr * 1.5) # 19
-    return np.where((data_out > upper_bound) | (data_out < lower_bound))
+    Q1 = data_out.quantile(0.25)
+    Q3 = data_out.quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    outlier_indices = np.where((data_out < lower_bound) | (data_out > upper_bound))
+    outlier_values = data_out.iloc[outlier_indices]
+    return outlier_values, outlier_indices
 
 # Example usage
 aaa = pd.DataFrame({'col1': [-10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 50], 
                     'col2': [100, 200, -30, 400, 500, 600, -70000, 800, 900, 1000, 210, 429, 350]})
-outliers_loc = outliers(aaa)
-print("이상치:", outliers_loc)
+outlier_values, outlier_indices = outliers(aaa)
+print("Outlier values:")
+print(outlier_values)
+print("Outlier indices:")
+print(outlier_indices)
+
+import matplotlib.pyplot as plt
+plt.boxplot(aaa)
+plt.show()
+
 
