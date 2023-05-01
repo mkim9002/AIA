1c650ab96fc1bb8e1b7d2fceab24163a2e269d71
mkim9002 <mkim9002@gmail.com>
2023-05-01 17:50:21 +0900

    wr
    wrrw

--- ml/m50_factory3_submit.py
+++ ml/m50_factory3_submit.py
@@ -134,22 +134,22 @@ print(x, '\n', y)
 
 
 x_train, x_test, y_train, y_test= train_test_split(
-    x,y, train_size=0.8, random_state=282, shuffle=True
+    x,y, train_size=0.8, random_state=123, shuffle=True
 ) 
 
-parameter = {'n_estimators' : 20000,
+parameter = {'n_estimators' : 10000,
               'learning_rate' : 0.07,   # 이게 성능이 가장 좋다
               'max_depth' : 3,
-              'gamma' : 1,
-              'min_child_weight' : 1,
-              'subsample' : 0.7,
-              'colsample_bytree' : 1,
-              'colsample_bylevel' : 1,
-              'colsample_bynode' : 1,
-              'reg_alpha' : 0,
-              'reg_lambda' : 0.01,
-              'random_state' : 1234,
-              'verbose' :0,
+            #   'gamma' : 1,
+            #   'min_child_weight' : 1,
+            #   'subsample' : 0.7,
+            #   'colsample_bytree' : 1,
+            #   'colsample_bylevel' : 1,
+            #   'colsample_bynode' : 1,
+            #   'reg_alpha' : 0,
+            #   'reg_lambda' : 0.01,
+            #   'random_state' : 1234,
+            #   'verbose' :0,
               'n_jobs': '-1'
               }
 
@@ -192,7 +192,7 @@ answer_sample_csv = pd.read_csv(path + 'answer_sample.csv',
 answer_sample_csv['PM2.5'] = y_submit
 print(answer_sample_csv)
 
-answer_sample_csv.to_csv(path + 'm50_factory3_submit.csv',
+answer_sample_csv.to_csv(path + 'm50_factory4_submit.csv',
                          index=None)
 
 
---
+++ ml/m51__bagging2.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+#1. 데이터
+x, y = load_breast_cancer(return_X_y=True)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=123, train_size=0.8, stratify=y)
+
+scaler  = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+#2. model
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+
+# model = DecisionTreeClassifier()
+# model = RandomForestClassifier()
+aaa = LogisticRegression()
+model = BaggingClassifier(aaa,
+                          n_estimators=10,
+                          n_jobs=-1,
+                          random_state=337,
+                          bootstrap=True,     # 디폴트
+                        #   bootstrap=False,
+                          
+                          )
+
+#3.훈련
+model.fit(x_train,y_train)
+
+#4.평가
+y_pred = model.predict(x_test)
+print('model.score :', model.score(x_test,y_test))
+print('acc :', accuracy_score(y_test,y_pred))
+
+
+#BaggingClassifier model.score :  0.9912280701754386   acc :  0.9912280701754386
+#RandomForestClassifier model.score : 0.956140350877193  acc : 0.956140350877193
+#DecisionTreeClassifier : model.score : 0.8947368421052632 acc : 0.8947368421052632
+# LogisticRegression  : model.score : 0.973684       acc : 0.973684  
+
