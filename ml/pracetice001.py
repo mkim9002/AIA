88b86291149b3b0c39094d56de490915afbc6f39
mkim9002 <mkim9002@gmail.com>
2023-05-05 18:49:48 +0900

    qweq
    adad

--- ml/pracetice001.py
+++ ml/pracetice001.py
@@ -1,10 +1,28 @@
 import numpy as np
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_breast_cancer
 
-#1. 데이터
-datasets = load_iris()
-x = datasets.data
-y = datasets['target']
-x,y = load_iris(return_X_y=True)
+#1.데이터
+x,y =load_breast_cancer(return_X_y=True)
 
-print(x.shape,y.shape)
+print(x.shape, y.shape) #(569, 30) (569,)
+
+#2, 모델
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+# model =LinearSVC() #0.9209138840070299
+# model =LogisticRegression() #0.9472759226713533
+# model = DecisionTreeClassifier() #1.0
+model = RandomForestClassifier()
+
+# 3.컴파일 훈련
+model.fit(x,y)
+
+#4, 훈련 평가
+results = model.score(x,y)
+
+print(results)
