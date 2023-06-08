88e567a62962f3ce3d4dd3c0589271ab06864dbe
Steven Kim <126454272+mkim9002@users.noreply.github.com>
2023-06-08 09:01:05 +0900

    Update pracetice프로야구3.py

--- ml/pracetice프로야구3.py
+++ ml/pracetice프로야구3.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler,RobustScaler,MaxAbsScaler,StandardScaler
 from sklearn.metrics import r2_score, mean_squared_error
 
-
+####
 # 1. data
 # 1.1 path, path_save, read_csv
 path = 'c:/study/_data/baseball/'
